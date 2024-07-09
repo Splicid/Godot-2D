376051ba2248c3aa28de7fbc12c73b47d626590d
Luis a <luis1abreu11@gmail.com>
2024-07-08 21:04:29 -0400

    ,

--- scripts/playerParent.gd
+++ scripts/playerParent.gd
@@ -1,11 +1,10 @@
 extends CharacterBody2D
 
+var bullet_scene: PackedScene = preload("res://scene/bullet.tscn")
+
 
 func _process(_delta):
 	var direction = Input.get_vector('left', 'right', 'up', 'down')
 	velocity = direction * 300
 	move_and_slide()
 	
-	# Shooting Input
-	if Input.is_action_pressed('fire'):
-		print('Shooting')
